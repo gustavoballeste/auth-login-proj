397dcd6bc2000619a714dd9481c543857b4582a6
gustavoballeste <gustavoballeste@gmail.com>
2017-11-21 15:55:26 -0200

    style: String resources

--- app/src/main/java/com/gustavoballeste/authlogin/detail/DetailActivity.java
+++ app/src/main/java/com/gustavoballeste/authlogin/detail/DetailActivity.java
@@ -47,8 +47,8 @@ public class DetailActivity extends AppCompatActivity implements DetailIView {
     @OnClick(R.id.first_name_card_view)
     public void showFirstNameDialog() {
         new MaterialDialog.Builder(this)
-                .title(R.string.first_name_input)
-                .content(R.string.first_name_input_content)
+                .title(R.string.first_name_dialog_title)
+                .content(R.string.first_name_dialog_label)
                 .inputType(
                         InputType.TYPE_CLASS_TEXT
                                 | InputType.TYPE_TEXT_VARIATION_PERSON_NAME
@@ -66,8 +66,8 @@ public class DetailActivity extends AppCompatActivity implements DetailIView {
     @OnClick(R.id.last_name_card_view)
     public void showLastNameDialog() {
         new MaterialDialog.Builder(this)
-                .title(R.string.last_name_input)
-                .content(R.string.last_name_input_content)
+                .title(R.string.last_name_dialog_title)
+                .content(R.string.last_name_dialog_label)
                 .inputType(
                         InputType.TYPE_CLASS_TEXT
                                 | InputType.TYPE_TEXT_VARIATION_PERSON_NAME
@@ -79,25 +79,6 @@ public class DetailActivity extends AppCompatActivity implements DetailIView {
                         this.lastNameTv.getText(),
                         false,
                         (dialog, input) -> this.presenter.updateValue(input.toString(), lastNameTv,"lastname"))
-                .show();
-    }
-
-    @OnClick(R.id.password_card_view)
-    public void showPasswordDialog() {
-        new MaterialDialog.Builder(this)
-                .title(R.string.password_input)
-                .content(R.string.password_input_content)
-                .inputType(
-                        InputType.TYPE_CLASS_TEXT
-                                | InputType.TYPE_TEXT_VARIATION_PERSON_NAME
-                                | InputType.TYPE_TEXT_FLAG_CAP_WORDS)
-                .inputRange(6, 30)
-                .positiveText(R.string.submit)
-                .input(
-                        null,
-                        null,
-                        false,
-                        (dialog, input) -> this.presenter.updateValue(input.toString(), passwordTv,"password"))
                 .show();
     }
 }
